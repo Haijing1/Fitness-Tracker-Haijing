a75f94d2edfc76c5fa3e5845c7495aa3aff63c5f
Haijing <Haijing.wang@hotmail.com>
2024-08-13 22:37:43 -0700

    add mixins and variables

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../src/components/Header/Header";
+
+// import Footer from "../src/components/Footer/Footer";
+// import InventoryPage from "./pages/InventoryPage/InventoryPage.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          {/* <Route path="/add-inventory" element={<InventoryPage />} />
+
+          <Route path="/warehouses" element={<HomePage />} />
+          <Route path="/warehouse/:warehouseId" element={<HomePage />} />
+          <Route path="/warehouse/addNewWarehouse" element={<HomePage />} />
+          <Route path="/warehouse/editWarehouse/:warehouseId" element={<HomePage />} />
+          <Route path="/inventories" element={<InventoryPage />} />
+
+
+          <Route path="/inventories/:inventoryId" element={<InventoryPage />} />
+          <Route path="/inventories/update/:inventoryId" element={<InventoryPage />}/> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
