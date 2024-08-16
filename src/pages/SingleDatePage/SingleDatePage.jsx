b1b1aaa834791678464ebb432a6bfafd92057f2e
Haijing <Haijing.wang@hotmail.com>
2024-08-16 09:35:49 -0700

    place data on single date page

--- src/pages/SingleDatePage/SingleDatePage.jsx
+++ src/pages/SingleDatePage/SingleDatePage.jsx
@@ -1,35 +1,32 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios"
 import "./SingleDatePage.scss";
-
 function SingleDatePage() {
     const date = new Date();
     const day = date.getDate();
     const month = date.getMonth() + 1; // The month index starts from 0
     const year = date.getFullYear();
     const formetDate = `${day}-${month}-${year}`;
-    // const [currentDate, setCurrentDate] = useState(formetDate);
+    const [currentDate, setCurrentDate] = useState(formetDate);
     const [workoutData, setWorkoutData] = useState(null);
     const baseApiUrl = import.meta.env.VITE_API_URL;
     const { dateId } = useParams();
     console.log(dateId)
-
     const handelDateChange = async (event) => {
         const date = new Date(event.target.value);
         const day = date.getDate();
         const month = date.getMonth() + 1; // The month index starts from 0
         const year = date.getFullYear();
-        const formetDate = `${day}-${month}-${year}`;
-        console.log(formetDate)
+        const inputDate = `${day}-${month}-${year}`;
+        console.log(inputDate)
         try {
-            const response = await axios.get(`${baseApiUrl}/workout/${formetDate}`);
+            const response = await axios.get(`${baseApiUrl}/workout/${inputDate}`);
             setWorkoutData(response.data)
         } catch (error) {
             console.error("Error getting single date data:", error)
         }
     }
-
     // const workoutData = [{
     //     "id": "7eb112d7-37e0-4ef6-b96c-9612559e039a",
     //     "timeStamp": "2024-08-15T23:29:27.482Z",
@@ -51,23 +48,20 @@ function SingleDatePage() {
     //         }
     //     ]
     // }]
-
     // useEffect(() => {
     //     async function getSingleDate(id) {
     //         try {
-    //             const response = await axios.get(`${baseApiUrl}/workout/${dateId}`);
+    //             const response = await axios.get(`${baseApiUrl}/workout/${id}`);
+    //             console.log(response.data)
     //             setWorkoutData(response.data)
     //         } catch (error) {
     //             console.error("Error getting single date data:", error)
     //         }
     //     }
-    //     if (dateId) {
-    //         getSingleDate(dateId)
-    //     } else {
-    //         //   getSingleVideoData(defaultId)
-    //     }
-    // }, [dateId]);
-    // console.log(dateId)
+    //     getSingleDate(formetDate)
+    // }, []);
+
+    console.log(dateId)
     console.log(workoutData)
 
     return (
@@ -95,11 +89,13 @@ function SingleDatePage() {
                     )
                 })}
             </div>
-
-
+            <div className="footer">
+                <Link to={`/add-exercise`}>
+                    <button className="add-button">+</button>
+                </Link>
+            </div>
         </div>
     )
 }
-
 
 export default SingleDatePage
